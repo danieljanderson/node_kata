299f3dcb0422f05c9d0aa0ae53b2dc88ba516779
danieljanderson <danieljanderson0616@gmail.com>
2019-02-22 15:11:20 -0500

    Add test harness

---
+++ katas/fizz-buzz/fizz-buzz.test.js
@@ -0,0 +1,33 @@
+/*
+# Three Laws of Test Driven Development
+- You are not allowed to write any production code unless it is to make a failing unit test pass.
+- You are not allowed to write any more of a unit test than is sufficient to fail; and compilation failures are failures.
+- You are not allowed to write any more production code than is sufficient to pass the one failing unit test.
+*/
+
+/*
+D - Don't
+R - Repeat
+Y - Yourself
+*/
+
+/*
+# ZOMBIES
+Z - Zero
+O - One
+M - Many
+B - Boundary Behavior
+I - Interface Definition
+E - Exercise Exception Behavior
+S - Simple
+*/
+
+const assert = require('assert');
+
+// test suite
+describe('Fizz Buzz', function() {
+  // individual test
+  it('', function() {
+    assert.equal(false, true)
+  });
+});
